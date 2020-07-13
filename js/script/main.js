7aa526613cc021cc5fd9e603de73bdf8a589c26a
kimi-ell <60607789+kimi-ell@users.noreply.github.com>
2020-07-13 22:18:18 +0300

    Add files via upload

--- js/script/main.js
+++ js/script/main.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
         $('.header-nav-btn__menu,.header-nav-menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $('.header-nav-menu-list').click(function(event){
+        $('.header-nav-btn__menu,.header-nav-menu').removeClass('active');
+        $('body').removeClass('lock')
+    });
+        
 });
